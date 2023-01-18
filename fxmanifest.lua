78f1c3c886a247909c47911a7451680bc14cc14b
Dark DESIGNS <123025862+DARK-DESIGNS@users.noreply.github.com>
2023-01-18 22:39:35 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DARK DESIGNS'
+description 'If you have any questions or need more information, feel free to join my discord: https://discord.gg/J5EEGnfhuE'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
